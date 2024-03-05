ae496b908586d13b947a0db18e9b752bffa6e9c1
Joel G. Stadelman <gonzalezstadelmanjoel@gmail.com>
2024-03-04 21:40:43 -0300

    Mas Listas

--- Listas.py
+++ Listas.py
@@ -44,4 +44,78 @@ print(name)
 
 print(my_list + my_other_list) # concatenamos dos listas
 
+my_list = "Hola python"
+print(my_list)
+print(type(my_list)) # una forma de ver el tipado dinamico(debilmente tipado), anteriormente 'my_list' era una variable tipo lista y ahora se cambió a una variable de tipo string.
+
+"""
+"en Python no se pueden crear constantes", pero se pueden crear variables finales, es decir,
+una buena practica para definir una variable como 'constante' es escribir dicha variable en MAYUSCULAS,
+de este modo quien opere con el codigo puede identificar dichas variables, esto es una regla no escrita.
+"""
+
+# Slicing Items from a List
+
+"""
+Positive Indexing: We can specify a range of positive indexes by specifying the start, 
+end and step, the return value will be a new list. 
+(default values for start = 0, end = len(lst) - 1 (last item), step = 1)
+"""
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+all_fruits = fruits[0:4] # it returns all the fruits
+# this will also give the same result as the one above
+all_fruits = fruits[0:] # if we don't set where to stop it takes all the rest
+orange_and_mango = fruits[1:3] # it does not include the first index
+orange_mango_lemon = fruits[1:]
+orange_and_lemon = fruits[::2] # here we used a 3rd argument, step. It will take every 2cnd item - ['banana', 'mango']
+
+"""
+Negative Indexing: We can specify a range of negative indexes by specifying the start, 
+end and step, the return value will be a new list.
+"""
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+all_fruits = fruits[-4:] # it returns all the fruits
+orange_and_mango = fruits[-3:-1] # it does not include the last index,['orange', 'mango']
+orange_mango_lemon = fruits[-3:] # this will give starting from -3 to the end,['orange', 'mango', 'lemon']
+reverse_fruits = fruits[::-1] # a negative step will take the list in reverse order,['lemon', 'mango', 'orange', 'banana']
+
+
+# Operando con listas
+
+my_other_list.append("GST.inc")
+print(my_other_list)
+
+my_other_list.insert(1, "amarillo")
+print(my_other_list)
+
+my_other_list.remove("amarillo")
+print(my_other_list)
+
+my_pop_element = fruits.pop() # la funcion 'pop' desapila el ultimo elemento.
+print(my_pop_element)
+print(fruits)
+
+del fruits[2] # elimina el elemento de la posicion indicada.
+print(fruits)
+
+my_new_list = fruits.copy()
+
+fruits.clear()
+print(fruits)
+print(my_new_list)
+
+my_new_list.reverse()
+print(my_new_list)
+
+my_new_list = ['orange', 'banana', 'mango', 'lemon']
+print(my_new_list)
+my_new_list.sort()
+print(my_new_list)
+
+
+# Sublistas
+
+print(my_new_list[1:3]) # conociendo el indice se pueden hacer sublistas.
 
