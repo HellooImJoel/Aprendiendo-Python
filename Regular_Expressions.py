aa0689acf5aaf13a0f1f43664bf3f43f658da3c6
Joel G. Stadelman <145398428+HellooImJoel@users.noreply.github.com>
2024-07-18 18:10:28 -0300

    Regular Expressions

--- Regular_Expressions.py
+++ Regular_Expressions.py
@@ -19,7 +19,7 @@ re.sub: Replaces one or many matches within a string
 
 import re
 
-my_string = "Esta es la leccion numero 7: Expresiones Regulares"
+my_string = "Esta es la leccion numero 7: Leccion llamada Expresiones Regulares"
 my_other_string = "Esta no es la leccion numero 6: Manejo de Ficheros"
 
 #print(re.match("Esta es la leccion", my_string, re.I))
@@ -42,22 +42,68 @@ if match is not None:
     print(my_other_string[start:end])
 
 
+## search
+
+search = re.search("leccion", my_string, re.I)
+print(search)
+start, end = search.span()
+print(my_string[start:end])
+
+
+## findall
+
+findall = re.findall("leccion", my_string, re.I)
+print(findall)
+
+my_new_string = "leccion, Leccion, LEccion, LECcion, LECCion,LECCión, LECCIon, LECCIOn, LECCION, LECCiÓN"
+
+findall = re.findall("[l|L][e|E][c|C][c|C][i|I][o|ó|O|Ó][n|N]", my_new_string, re.I)
+print(findall)
+
+## split
+
+split = re.split(":", my_string)
+print(split)
+
+
+## sub
+
+sub = re.sub("Expresiones Regulares","RegEx", my_string)
+print(sub)
+
+sub = re.sub("[l|L]eccion","LECCION", my_string)
+print(sub)
 
 
 
+## Patterns
 
+pattern = r'[lL]eccion'
+print(re.findall(pattern, my_string))
 
+pattern = r'[lL]eccion|Expresiones'
+print(re.findall(pattern, my_string))
 
+pattern = r'[a-z]'
+print(re.findall(pattern, my_string))
 
+pattern = r'[0-9]'
+print(re.findall(pattern, my_string))
 
+pattern = r"\d"
+print(re.findall(pattern, my_string))
 
+pattern = r"\D"
+print(re.findall(pattern, my_string))
 
+pattern = r"[l].*"
+print(re.findall(pattern, my_string))
 
+email = "gonzalezstadelmanjoel@gmail.com"
+pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z-.]+$"
+print(re.match(pattern, email))
+print(re.search(pattern, email))
+print(re.findall(pattern, email))
 
-
-
-
-
-
-
-
+email = "joeldevv@joeldev.com.ar"
+print(re.findall(pattern, email))
